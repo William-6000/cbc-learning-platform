42e100316ec9a8a50a3952dbfcedce2ef4277121
William-6000 <williamkitoe7@gmail.com>
2026-06-12 11:25:51 +0300

    Create i18n.js

---
+++ client/src/utils/i18n.js
@@ -0,0 +1,4 @@
+export const translations = {
+  en: { dashboard: 'Dashboard', pathways: 'Pathways', subjects: 'Subjects', assessments: 'Assessments', progress: 'Progress', complete: 'Mark complete', login: 'Login' },
+  sw: { dashboard: 'Dashibodi', pathways: 'Mikondo', subjects: 'Masomo', assessments: 'Tathmini', progress: 'Maendeleo', complete: 'Kamilisha', login: 'Ingia' }
+};
